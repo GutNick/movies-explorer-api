72c8e165676d4440d8e54752e7df3d158d2566d1
GutNick <chelyabinsk.it@gmail.com>
2022-04-25 15:22:40 +0500

    Fix after first review

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -7,18 +7,18 @@ module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    next(new UnauthorizedError('Необходима авторизация'));
-  } else {
-    const token = authorization.replace('Bearer ', '');
-    let payload;
-    try {
-      payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'brilliant-secret-key');
-    } catch (err) {
-      next(new UnauthorizedError('Необходима авторизация'));
-    }
-
-    req.user = payload;
-
-    next();
+    return next(new UnauthorizedError('Необходима авторизация'));
   }
+
+  const token = authorization.replace('Bearer ', '');
+  let payload;
+  try {
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'brilliant-secret-key');
+  } catch (err) {
+    next(new UnauthorizedError('Необходима авторизация'));
+  }
+
+  req.user = payload;
+
+  return next();
 };
